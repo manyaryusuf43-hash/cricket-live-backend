b0e31e26f174efadce0f2447e874047a506c750e
manyaryusuf43-hash <manyaryusuf43@gmail.com>
2026-05-19 23:43:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
+
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {

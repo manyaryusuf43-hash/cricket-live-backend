1c7b4547aa46f111e524f4a94452de936fe454d6
manyaryusuf43-hash <manyaryusuf43@gmail.com>
2026-05-22 00:20:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,27 +11,36 @@ app.get("/", (req, res) => {
 });
 
 app.get("/live", async (req, res) => {
+  try {
 
-  const response = await axios.get(
-  "https://site.api.espn.com/apis/site/v2/sports/cricket/scoreboard"
-);
+    const response = await axios.get(
+      "https://site.api.espn.com/apis/site/v2/sports/cricket/scoreboard"
+    );
 
-const matches = response.data.events.map((match, index) => ({
-  id: index + 1,
-  team1: match.competitions[0].competitors[0].team.displayName,
-  team2: match.competitions[0].competitors[1].team.displayName,
-  status: match.status.type.detail,
-  score:
-    match.competitions[0].competitors[0].score +
-    "/" +
-    match.competitions[0].competitors[1].score
-}));
+    const matches = response.data.events.map((match, index) => ({
+      id: index + 1,
+      team1: match.competitions[0].competitors[0].team.displayName,
+      team2: match.competitions[0].competitors[1].team.displayName,
+      status: match.status.type.detail,
+      score:
+        match.competitions[0].competitors[0].score +
+        "/" +
+        match.competitions[0].competitors[1].score
+    }));
 
-  res.json({
-    success: true,
-    matches: matches
-  });
+    res.json({
+      success: true,
+      matches
+    });
 
+  } catch (err) {
+
+    res.json({
+      success: false,
+      error: err.message
+    });
+
+  }
 });
 
 const PORT = process.env.PORT || 10000;
